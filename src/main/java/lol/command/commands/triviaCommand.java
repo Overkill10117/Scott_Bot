ad2ab9c63eb10df79b7063de6377d52241fbccd4
Overkill10117 <77133161+Overkill10117@users.noreply.github.com>
2021-02-18 13:50:52 +0800

    some changes....

--- src/main/java/lol/command/commands/triviaCommand.java
+++ src/main/java/lol/command/commands/triviaCommand.java
@@ -6,7 +6,19 @@ import lol.command.ICommand;
 public class triviaCommand implements ICommand {
     @Override
     public void handle(CommandContext ctx) {
-        ctx.getChannel().sendMessage("scott loves calling his brother and enemies 'gay' ").queue();
+        double meh = Math.random()*10;
+
+        switch ((int) meh) {
+            case 1:
+                ctx.getChannel().sendMessage("scott loves calling his brother and enemies \"gay\"\n").queue();
+                return;
+            case 2:
+                ctx.getChannel().sendMessage("scott is a human....").queue();
+                return;
+            case 3:
+                ctx.getChannel().sendMessage(" scott is :scream: ing").queue();
+                return;
+        }
     }
 
     @Override
